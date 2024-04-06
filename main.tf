5d013e68222903e854655bcc6cc645f262cc58ba
Luis Castellanos <126070649+Luis75629@users.noreply.github.com>
2024-04-06 20:36:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ resource "aws_security_group" "instance_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["192.168.1.35/32"]
+    cidr_blocks = ["xxx.xxx.x.xx/32"]
   }
   
   egress {
